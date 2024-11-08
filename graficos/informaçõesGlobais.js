8590a6f8fd9584713486f3123e2a37ab7b74611b
AquieGTA12 <133871490+AquieGTA12@users.noreply.github.com>
2024-11-08 08:01:32 -0300

    .

--- graficos/informaçõesGlobais.js
+++ graficos/informaçõesGlobais.js
@@ -3,9 +3,10 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function vizualizarinformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
+    console.log(dados);
     const paragrafo document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = ``
+    paragrafo.innerHTML = `Você sabia que no planeta há ${dados.total_pessoas_mundo} de pessoas`
 }
 
 vizualizarinformacoesGlobais()
